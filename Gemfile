3facc646a9fd700b2d08da252d08412694b916ec
Remya Senan <remya.senan@rubyians.com>
2018-12-09 23:57:10 +0000

    removed some irrelevant code

--- Gemfile
+++ Gemfile
@@ -1,10 +1,5 @@
 source 'https://rubygems.org'
 
-git_source(:github) do |repo_name|
-  repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
-  "https://github.com/#{repo_name}.git"
-end
-
 gem 'rails', '~> 5.1.6', '>= 5.1.6.1'
 gem 'sqlite3'
 gem 'puma', '~> 3.7'
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,22 +1,11 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 
-  include Tokenable
-
-  def index
-    if params[:approved] == "false"
-      @users = User.find_all_by_approved(false)
-    else
-      @users = User.all
-    end
-  end
-
   def new
     @user = User.new
   end
 
   def create
     @user = User.new(user_params)
-
     respond_to do |format|
       if resource.save
        format.json { render json: @user, status: :created, location: @user }
