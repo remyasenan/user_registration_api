52b7a4337a9258f6780f693adb8ff0200b5b414d
Remya Senan <remya.senan@rubyians.com>
2018-12-10 00:06:05 +0000

    added devise friendly_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,10 @@
 class User < ApplicationRecord
-  include Tokenable
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   def after_database_authentication
-    @token = Tokenable.generate_token
+    @token = Devise.friendly_token
     current_user.user_token = @token
     current_user.token_valid_time_in_minutes = 60
     current_user.token_validity_end_by = Time.now + 60*60
